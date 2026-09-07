f72df78dd92b2fbf15daac149d089db7b45eda3c
rudraparekh04 <rudraparekh04@gmail.com>
2026-09-07 23:46:23 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -73,15 +73,15 @@ const iconMap = {
             transition={{ duration: 0.6, delay: 0.2 }}
             className="text-slate-400 leading-relaxed space-y-6 text-lg"
           >
-            <p>
-              Hello! My name is Jane and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together HTML & CSS taught me a lot about about layout and design!
+            {/* <p>
+              Hello! My name is Rudra and I enjoy creating things that live on the internet. My interest in web development started back in 2023 when I decided to try editing custom Tumblr themes — turns out hacking together HTML & CSS taught me a lot about about layout and design!
             </p>
             <p>
               Fast-forward to today, and I've had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences for a variety of clients.
             </p>
             <p>
               I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
-            </p>
+            </p> */}
           </motion.div>
 
           <motion.div
